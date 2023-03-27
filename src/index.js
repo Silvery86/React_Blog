4ef59ee0399fc8898c74a655b839dcbfd14dddfe
Silvery86 <giang8692@gmail.com>
2023-03-28 06:59:08 +0700

    Add Home and Navbar component, style the App component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const element = <h1>Hello World</h1>
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <App />,
   document.getElementById('root')
 );
 
