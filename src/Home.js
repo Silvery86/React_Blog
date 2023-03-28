7fb8b421685bb4e170aa516c33510a846f1867f7
Silvery86 <giang8692@gmail.com>
2023-03-29 06:40:05 +0700

    Check and catch error when connect local data sever

--- src/Home.js
+++ src/Home.js
@@ -5,18 +5,31 @@ const Home = () => {
 
 
     const [blogs, setBlogs] = useState(null);
- 
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
        
     useEffect(() =>{
-        fetch('http://localhost:3500/blogs').then(res =>{
+        fetch('http://localhost:3500/blogs')
+        .then(res =>{
+            if(!res.ok){
+                throw Error('could not fetch the data from that resource')
+            }
             return res.json();
         })
         .then(data => {
             setBlogs(data);
+            setIsLoading(false);
+            setError(null);
         })
+        .catch((err) =>{
+            setIsLoading(false);
+            setError(err.message);
+        });
     },[])
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading .....</div>}
             {blogs && <BlogList blogs={blogs} title={'All Blogs!'}/>}
             
         </div>
