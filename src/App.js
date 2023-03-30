3403e02c7171f1f479611e03bffb6feafcc459bf
Silvery86 <giang8692@gmail.com>
2023-03-30 07:07:48 +0700

    Add Router for React Web

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   
  
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-    </div>
+    <Router>
+          <div className="App">
+          <Navbar />
+          <div className="content">
+            <Switch>
+              <Route patch="/">
+                <Home/>
+              </Route>
+            </Switch>
+          </div>
+         </div>
+    </Router>
+    
   );
 }
 
