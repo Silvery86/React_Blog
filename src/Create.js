255dde6f74d67fa25af4e58e2a533a26bf7870c7
Silvery86 <giang8692@gmail.com>
2023-04-03 07:03:39 +0700

    Make delete funtion for delete blog in json file

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
 const Create = () => {
-const [title, setTitle] = useState('')
-const [body, setBody] = useState('')
-const [author, setAuthor] = useState('mario')
-const [isPending, setIsPending] = useState(false)
+const [title, setTitle] = useState('');
+const [body, setBody] = useState('');
+const [author, setAuthor] = useState('mario');
+const [isPending, setIsPending] = useState(false);
+const history = useHistory();
+
 
 const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,7 +22,10 @@ const handleSubmit = (e) => {
     }).then(() => {
         console.log('Blog added!')
         setIsPending(false);
+        history.push('/')
     })
+
+   
 }
     return ( 
         <div className="create">
